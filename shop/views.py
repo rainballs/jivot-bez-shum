5b47346270a42df05d64e03be5510419b9e1d49e
Nikolay Atsev <rainballs.niki@gmail.com>
2026-01-05 12:41:51 +0200

    price change

--- shop/views.py
+++ shop/views.py
@@ -326,7 +326,7 @@ def stripe_create_checkout_session(request):
             },
             success_url=success_url,  # ⬅️ now to thank_you
             cancel_url=stripe_cancel_url(request),
-            currency="bgn",
+            currency="eur",
             customer_email=order.email or None,
         )
     except Exception as e:
@@ -967,7 +967,7 @@ def stripe_create_checkout_session(request):
             },
             success_url=success_url,  # ⬅️ now to thank_you
             cancel_url=stripe_cancel_url(request),
-            currency="bgn",
+            currency="eur",
             customer_email=order.email or None,
         )
     except Exception as e:
